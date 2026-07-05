8d150f4188fece4b26ced74b34c21936626997be
Norhan Hamed <62403829+norhanhamed@users.noreply.github.com>
2026-07-05 16:16:04 +0300

    about-us section

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+
+// const boxcard = document.getElementById("boxcard");
+// const count = window.innerWidth <= 768 ? 12 : 9;
+
+
+// for( let i =0 ; i < count ; i++){
+//     const img = document.createElement("img");
+//     img.src = './imgs/about-us-3.png';
+//     img.alt= "user";
+//     boxcard.appendChild(img);
+// }
+
+
+
+const boxcard = document.getElementById("boxcard");
+
+function createImages() {
+    boxcard.innerHTML = "";
+
+    const count = window.innerWidth <= 768 ? 12 : 9;
+
+    for (let i = 0; i < count; i++) {
+        const img = document.createElement("img");
+        img.src = "./imgs/about-us-3.png";
+        img.alt = "user";
+        boxcard.appendChild(img);
+    }
+}
+
+createImages();
+window.addEventListener("resize", createImages);
